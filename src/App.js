565efcee47c787a53eed0af74894732917ddab0d
Me-cha <127575583+Me-cha@users.noreply.github.com>
2024-04-03 12:25:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import DemoComponent from "./DemoComponent";
-
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div>
-   <DemoComponent />
     </div>
   );
 }
